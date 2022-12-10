c9e1167f6b29e1381fae542ed56bc2d89416622a
Laucsap <110190482+actwu@users.noreply.github.com>
2022-12-10 16:07:47 +0800

    Add files via upload

---
+++ sets/db.js
@@ -0,0 +1,11 @@
+
+  database = [
+
+    site = "https://script.google.com/",
+    dbtype = "macros",
+    subject="/s/",
+
+    icsdb ="AKfycbzDcitAsahfvU6lOatXqQW17w7zw1Jw2RlIqtlZ3RPyZuCSWqNxujSZ5E0x7or0zpaO",
+    db = [`${site}${dbtype}${subject}${icsdb}/exec`]
+
+  ]
